5da40c834c01f6c7bfad17d74275c1f6e592aa6c
Mark <markhost@yandex.ru>
2015-09-19 18:28:53 +0300

    Новые поля для пользователя

--- models/Users.py
+++ models/Users.py
@@ -1,4 +1,8 @@
 import webapp2_extras.appengine.auth.models as auth_models
+from google.appengine.ext import ndb
 
 class User(auth_models.User):
+    name = ndb.StringProperty()
+    email = ndb.StringProperty()
+    phone = ndb.IntegerProperty()
     pass
