d6e60ba0f462a2a91f432a1fa3e33ccda62af185
Mark <markhost@yandex.ru>
2015-09-20 11:21:12 +0300

    Перфекционизация верстки

--- tmpls/js/script.js
+++ tmpls/js/script.js
@@ -1,10 +1,10 @@
-function FormController($scope) {
+function loginFormController($scope) {
     $scope.submit = function () {
 
     };
     $scope.checkname = function () {
         if ($scope.LoginFormData.login.length > 3) {
-
+            console.warn($scope);
         }
     }
 }
